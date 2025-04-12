a69af36842ec072312adb43d64a885467f2c6d05
Сергей <smolanovsergey@gmail.com>
2025-04-12 17:05:56 +0300

    add

--- wp-content/themes/agency_917/index.php
+++ wp-content/themes/agency_917/index.php
@@ -5,7 +5,7 @@ get_header();
 ?>
 
     <main class="main">
-<!--        --><?php //get_template_part( 'template-parts/content', 'banner' ); ?>
+        <?php get_template_part( 'template-parts/content', 'banner' ); ?>
     </main>
 
 <?php
